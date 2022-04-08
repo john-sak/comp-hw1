63371fe1b1b13f178f120fe3f6d153a7b09b3301
JohnSak <giannossak@gmail.com>
2022-04-09 01:29:26 +0300

    new prompt in Main.java

--- part2/main/Main.java
+++ part2/main/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 class Main {
     public static void main(String[] argv) throws Exception{
-        System.out.println("Please type your arithmethic expression:");
+        System.out.println("Please type your program in the input language below:");
         Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
         p.parse();
     }
