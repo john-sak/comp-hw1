c95fe8f0b19803bbec3e69db1db2d4d2226483ee
JohnSak <giannossak@gmail.com>
2022-04-09 01:36:28 +0300

    identation fixes

--- part2/main/Main.java
+++ part2/main/Main.java
@@ -2,7 +2,7 @@ import java_cup.runtime.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] argv) throws Exception{
+    public static void main(String[] argv) throws Exception {
         System.out.println("Please type your program in the input language below:");
         Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
         p.parse();
